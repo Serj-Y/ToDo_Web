48098c133ead42f892e18725a44017b3bc1e08d4
Serj-Y <126787294+Serj-Y@users.noreply.github.com>
2024-04-15 17:39:34 +0300

    Add yupValidation and build firs validation in change userName

--- src/feautures/EditUser/ui/ChangeUserNameForm/ChangeUserNameForm.tsx
+++ src/feautures/EditUser/ui/ChangeUserNameForm/ChangeUserNameForm.tsx
@@ -5,6 +5,8 @@ import { Button, ButtonSize } from 'shared/ui/Button/Button';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { Controller, useForm } from 'react-hook-form';
 import { Text, TextAlign, TextTheme } from 'shared/ui/Text/Text';
+import * as yup from 'yup';
+import { useYupValidationResolver } from 'shared/lib/hooks/useYupValidationResolver/useYupValidationResolver';
 import cls from './ChangeUserNameForm.module.scss';
 import { changeUserName } from '../../model/services/changeUserName/changeUserName';
 
@@ -16,31 +18,43 @@ export interface ChangeUserNameFormProps {
 interface FormData {
     userName: string
 }
-
 const ChangeUserNameForm = memo(({ className, currentName }: ChangeUserNameFormProps) => {
     const { t } = useTranslation();
-    const { control, handleSubmit, reset } = useForm<FormData>();
     const dispatch = useAppDispatch();
+    const validationSchema = yup.object({
+        userName: yup.string()
+            .required(t('This field is required'))
+            .min(3, t('Minimum 3 symbols'))
+            .max(15, t('Maximum 15 symbols'))
+        ,
+    });
+    const {
+        register, control, handleSubmit, formState: { errors }, reset,
+    } = useForm<FormData>({
+        resolver: useYupValidationResolver(validationSchema),
+    });
 
     const onSubmit = useCallback((data: FormData) => {
         dispatch(changeUserName({ name: data.userName }));
         reset();
     }, [dispatch, reset]);
+
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={cls.ChangeUserNameForm}>
             <Text title={t('Change user name')} align={TextAlign.CENTER} />
             <div className={cls.currentName}>
                 <Text text={t('Current user name')} />
                 <Text text={currentName} />
+                <Text text={errors.userName?.message} theme={TextTheme.ERROR} align={TextAlign.CENTER} />
             </div>
             <Controller
                 name="userName"
                 control={control}
                 defaultValue=""
-                rules={{ minLength: 2, maxLength: 50 }}
                 render={({ field }) => (
                     <Input
                         {...field}
+                        {...register('userName')}
                         customPlaceholder={t('Enter new user name')}
                         placeholder={t('Enter new user name')}
                         onChange={(value) => field.onChange(value)}
---
+++ src/shared/lib/hooks/useYupValidationResolver/useYupValidationResolver.ts
@@ -0,0 +1,32 @@
+import { useCallback } from 'react';
+
+export const useYupValidationResolver = (validationSchema: any) => useCallback(
+    async (data: any) => {
+        try {
+            const values = await validationSchema.validate(data, {
+                abortEarly: false,
+            });
+
+            return {
+                values,
+                errors: {},
+            };
+        } catch (errors) {
+            return {
+                values: {},
+                // @ts-ignore
+                errors: errors.inner.reduce(
+                    (allErrors: any, currentError: { path: any; type: any; message: any; }) => ({
+                        ...allErrors,
+                        [currentError.path]: {
+                            type: currentError.type ?? 'validation',
+                            message: currentError.message,
+                        },
+                    }),
+                    {},
+                ),
+            };
+        }
+    },
+    [validationSchema],
+);
