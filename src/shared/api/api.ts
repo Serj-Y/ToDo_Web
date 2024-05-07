2a79c3ecb1131f44a805cc9a94a8c0e7b57454c0
Serj-Y <126787294+Serj-Y@users.noreply.github.com>
2024-05-08 00:58:56 +0300

    Fix api undefined status error

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -58,9 +58,8 @@ $api.interceptors.response.use(
     (config) => config,
     async (error) => {
         const originalRequest = error.config;
-
         if (
-            (error.response.status === 401
+            (error.response?.status === 401
                 || errorCatch(error) === 'jwt expired'
                 || errorCatch(error) === 'jwt must be provided')
             && error.config
